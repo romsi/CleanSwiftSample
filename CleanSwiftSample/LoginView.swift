d9875ada99aefbfca2784a569dd403177a555263
Romain Asnar <romain.asnar@gmail.com>
2021-11-12 11:48:34 +0100

    feat(login): Add login feature with Firebase.

--- CleanSwiftSample/LoginView.swift
+++ CleanSwiftSample/LoginView.swift
@@ -6,12 +6,49 @@
 //
 
 import SwiftUI
+import Firebase
+
+struct AlertContent: Identifiable {
+	var id: String
+	var title: String
+	var description: String
+}
 
 struct LoginView: View {
+	@State private var email: String = ""
+	@State private var password: String = ""
+	@State private var alertContent: AlertContent?
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		VStack(alignment: .leading) {
+			TextField("Email", text: $email)
+			SecureField("Password", text: $password)
+			Button("Login") { login(email: email, password: password) }
+		}
+		.alert(item: $alertContent) { content in
+			Alert(
+				title: Text(content.title),
+				message: Text(content.description),
+				dismissButton: .cancel(Text("OK"), action: {
+					alertContent = nil
+				})
+			)
+		}
     }
+	
+	private func login(email: String, password: String) {
+		Auth.auth().signIn(withEmail: email, password: password) { authData, error in
+			guard let authData = authData else {
+				return
+			}
+			let name = authData.user.displayName!
+			alertContent = AlertContent(
+				id: UUID().uuidString,
+				title: "Welcome \(name)",
+				description: "Enjoy our great experience!"
+			)
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {

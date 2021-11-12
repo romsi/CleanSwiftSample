9520681b1316fb98834c11d82b1e64daa5ed06a4
Romain Asnar <romain.asnar@gmail.com>
2021-11-12 10:59:05 +0100

    chore(xcode): Create CleanSwiftSample project.

---
+++ CleanSwiftSample/CleanSwiftSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSwiftSampleApp.swift
+//  CleanSwiftSample
+//
+//  Created by Romain Asnar on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CleanSwiftSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
